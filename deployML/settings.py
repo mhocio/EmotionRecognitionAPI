4281f46435454828844f0a9385e575da4855e885
1203217 <jorgina.arres@est.fib.upc.edu>
2020-05-18 14:57:54 +0200

    Add my image face detection

--- deployML/settings.py
+++ deployML/settings.py
@@ -15,7 +15,6 @@ import os
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
@@ -27,7 +26,6 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
-
 # Application definition
 
 INSTALLED_APPS = [
@@ -38,6 +36,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'rest_framework_xml',
     'prediction'
 ]
 
@@ -72,7 +71,6 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'deployML.wsgi.application'
 
-
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
@@ -82,7 +80,6 @@ DATABASES = {
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/3.0/ref/settings/#auth-password-validators
@@ -102,7 +99,6 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-
 # Internationalization
 # https://docs.djangoproject.com/en/3.0/topics/i18n/
 
@@ -116,10 +112,18 @@ USE_L10N = True
 
 USE_TZ = True
 
-
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+REST_FRAMEWORK = {
+    'DEFAULT_PARSER_CLASSES': (
+        'rest_framework_xml.parsers.XMLParser',
+    ),
+    'DEFAULT_RENDERER_CLASSES': (
+        'rest_framework_xml.renderers.XMLRenderer',
+    ),
+}
--- prediction/views.py
+++ prediction/views.py
@@ -1,3 +1,5 @@
+import os
+
 import cv2
 
 from django.shortcuts import render
@@ -6,44 +8,76 @@ import numpy as np
 import tensorflow as tf
 from tensorflow_core.python.keras.models import model_from_json
 
-global graph,model
+global graph, model
 
-#initializing the graph
+# initializing the graph
 
-#loading our trained model
+# loading our trained model
 # load model
 model = model_from_json(open("AIModelv1.json", "r").read())
 # load weights
 model.load_weights('AIModelv1.h5')
 
-
-#creating a dictionary of classes
+# creating a dictionary of classes
 class_dict = {'angry': 0,
-            'disgust': 1,
-            'fear': 2,
-            'happy': 3,
-            'sad': 4,
-            'surprise': 5,
-            'neutral': 6
+              'disgust': 1,
+              'fear': 2,
+              'happy': 3,
+              'sad': 4,
+              'surprise': 5,
+              'neutral': 6
               }
 
 class_names = list(class_dict.keys())
+face_haar_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
+print(face_haar_cascade.empty())
+
 
 def prediction(request):
     if request.method == 'POST' and request.FILES['myfile']:
         post = request.method == 'POST'
         myfile = request.FILES['myfile']
-        img = image.load_img(myfile, target_size=(48, 48), grayscale=True)
-        #img = cv2.resize(img, (48, 48))
-        img = image.img_to_array(img)
-        img = np.expand_dims(img, axis=0)
-        preds = model.predict(img)
-        preds = preds.flatten()
-        m = max(preds)
-        for index, item in enumerate(preds):
-            if item == m:
-                result = class_names[index]
+
+        uploaded_file = request.FILES['myfile']
+        test_img = image.img_to_array(image.load_img(uploaded_file))
+        test_img = np.array(test_img, dtype='uint8')
+
+
+        #        im = np.fromstring(m, np.uint8)
+        gray_img = cv2.cvtColor(test_img, cv2.COLOR_BGR2GRAY)
+        face_haar_cascade = cv2.CascadeClassifier(
+            '/Users/jorginaarrescardona/PycharmProjects/deployML/prediction/haarcascade_frontalface_default.xml')
+        faces_detected = face_haar_cascade.detectMultiScale(gray_img, 1.32, 5)
+
+        for (x, y, w, h) in faces_detected:
+            cv2.rectangle(test_img, (x, y), (x + w, y + h), (255, 255, 0), thickness=5)
+            roi_gray = gray_img[y:y + w, x:x + h]  # cropping region of interest i.e. face area from  image
+            roi_gray = cv2.resize(roi_gray, (48, 48))
+            img_pixels = image.img_to_array(roi_gray)
+            img_pixels = np.expand_dims(img_pixels, axis=0)
+            # img_pixels /= 255
+            # uncoment if expected predict are normalized pixels
+
+            predictions = model.predict(img_pixels)
+
+            # find max indexed array
+            max_index = np.argmax(predictions[0])
+
+            emotions = ('angry', 'disgust', 'fear', 'happy', 'sad', 'surprise', 'neutral')
+            predicted_emotion = emotions[max_index]
+
+            cv2.putText(test_img, predicted_emotion, (int(x), int(y)), cv2.FONT_HERSHEY_SIMPLEX, 2, (255, 0, 0), 2)
+
+        # preds = model.predict(img)
+        # preds = preds.flatten()
+        # m = max(preds)
+        # for index, item in enumerate(preds):
+        #     if item == m:
+        #         result = class_names[index]
+
+        # resized_img = cv2.resize(m, (1000, 700))
+        # predictedimg= cv2.imshow('Facial emotion analysis ', m)
         return render(request, "prediction.html", {
-            'result': result})
+            'result': predicted_emotion})
     else:
         return render(request, "prediction.html")
